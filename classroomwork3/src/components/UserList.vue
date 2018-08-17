7ea8dfb6a4db0b1c43013b1c13046177ddeba42f
VladimirSolovyov <vovanchik3007@mail.ru>
2018-08-17 14:04:19 +0300

    добавил_vue.config

--- classroomwork3/src/components/UserList.vue
+++ classroomwork3/src/components/UserList.vue
@@ -1,42 +1,40 @@
 <template>
-    <table>
-        <thead>
-            <th>#</th>
-            <th>Имя</th>
-            <th>Фамилия</th>
-            <th>Активен</th>
-            <th>Баланс</th>
-            <th>Email</th>
-            <th>Телефон</th>
-            <th>Зарегистрирован</th>
-        </thead>
-        <tbody v-for="item in users" :key="item.id">
-            <td># {{ item.id }}</td>
-            <td>{{ item.firstName }}</td>
-            <td>{{ item.lastName }}</td>
-            <td>{{ item.isActive }}</td>
-            <td>{{ item.balance }}</td>
-            <td>{{ item.email }}</td>
-            <td>{{ item.phone }}</td>
-            <td>{{ item.registered }}</td>
-            
-        </tbody>
-            </table>
+  <table>
+    <thead>
+      <th>#</th>
+      <th>Имя</th>
+      <th>Фамилия</th>
+      <th>Активен</th>
+      <th>Баланс</th>
+      <th>Email</th>
+      <th>Телефон</th>
+      <th>Зарегистрирован</th>
+    </thead>
+    <tbody v-for="item in users" :key="item.id">
+      <td># {{ item.id }}</td>
+      <td>{{ item.firstName }}</td>
+      <td>{{ item.lastName }}</td>
+      <td>{{ item.isActive }}</td>
+      <td>{{ item.balance }}</td>
+      <td>{{ item.email }}</td>
+      <td>{{ item.phone }}</td>
+      <td>{{ item.registered }}</td>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
-    props: {
-        users: {
-            type: Array,
-            required: true
-        }
-    },
-    computed: {
-        total: function() {
-            return this.users.length
-        }
+  props: {
+    users: {
+      type: Array,
+      required: true
     }
-
+  },
+  computed: {
+    total: function() {
+      return this.users.length
+    }
+  }
 }
 </script>
--- classroomwork3/src/views/Users.vue
+++ classroomwork3/src/views/Users.vue
@@ -2,38 +2,48 @@
   <div>
     <h2>Список пользователей</h2>
 
-    <div v-if="!users.length" class="alert alert-warning">
-        Загрузка...
-        
+    <div v-if="loading" class="alert alert-warning">
+      Загрузка...        
+    </div>
+    <div v-if="!users.length">
+      Нет пользователей
     </div>
     <user-list v-else :users="users" />    
+
+    <button type="button" class="btn btn-primary" @click="loadUsers">
+      Обновить
+    </button>
+
   </div>
 </template>
 <script>
 import axios from 'axios'
-import UsersList from '@/components/UserList.vue'
+import UserList from '@/components/UserList.vue'
 
 export default {
-    components: {
-        'user-list': UsersList
-    },
-        data: function () {
-            return {
-                users: []
-            };
-        },
-        mounted: function () {
-            this.loadUsers();
-        },
-        methods: { 
-            loadUsers: function () {
-                var self = this;
+  components: {
+    UserList
+  },
+  data: () => ({
+    users: [],
+    loading: true
+  }),
 
-                axios.get('db.json').then(function (response) {
-                    self.users = response.data.users;
-                })
-            }
-        }
-       
+  mounted() {
+    this.loadUsers()
+  },
+  methods: {
+    loadUsers() {
+      this.loading = true
+
+      axios
+        .get('http://localhost:3000/users')
+        .then(response => {
+          this.users = response.data
+          this.loading = false
+        })
+        .catch(error => console.log(error))
     }
+  }
+}
 </script>
