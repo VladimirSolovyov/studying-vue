600e6e9d2a82c7324251046979e6e24bf6c728f9
VladimirSolovyov <vovanchik3007@mail.ru>
2018-08-27 17:01:55 +0300

    'Уроки_по_16.08'

--- classroomwork3/src/components/UserForm.vue
+++ classroomwork3/src/components/UserForm.vue
@@ -15,12 +15,36 @@
       <textarea v-model="localUser.about" type="text" class="form-control"/>
     </div>
 
+    <div class="form-group">
+      <label>URL картинки</label>
+      <br>
+      <img :src="localUser.picture" class="img-thumbnail">
+      <input
+        v-model="localUser.picture"
+        type="text"
+        class="form-control">
+      <input ref="avatar" type="file" class="hidden" @change="uploadAvatar">
+
+      <button type="button" class="btn btn-primary" @click="selectNewImage">
+        Загрузить новый
+      </button>
+    </div>
+
+    <div class="form-group">
+      <label>Дата регистрации - Закончил на 16.08 минута 36</label>
+      <datepicker v-model="localUser.registered" />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from '@/axios.js'
+
 export default {
   name: 'UserForm',
+  components: {
+    Datepicker: () => import('@/components/datepicker.vue')
+  },
   model: {
     prop: 'user',
     event: 'megaevent'
@@ -46,10 +70,36 @@ export default {
   methods: {
     updateUser() {
       this.$emit('megaevent', Object.assign({}, this.localUser))
+    },
+    selectNewImage() {
+      this.$refs.avatar.click()
+    },
+
+    uploadAvatar() {
+      const url = 'https://api.imgur.com/3/image'
+      const data = new FormData()
+      data.append('image', this.$refs.avatar.files[0])
+      // Добавляем ключ от IMGUR
+      // https://api.imgur.com/oauth2/addclient
+      const config = {
+        headers: {
+          Authorization: 'Client-ID 3bef0b8892d4b04'
+        }
+      }
+      axios
+        .post(url, data, config)
+        .then(response => response.data)
+        .then(response => {
+          this.localUser.picture = response.data.link
+          this.$refs.avatar.value = ''
+        })
     }
   }
 }
 </script>
 
 <style scoped>
+.hidden {
+  display: none;
+}
 </style>
---
+++ classroomwork3/src/components/datepicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input ref="input" :value="value" type="text" class="form-control">
+  </div>
+</template>
+
+<script>
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.css'
+
+export default {
+  name: 'Datepicker',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  watch: {
+      value: 'updatePicker'
+  },
+  mounted() {
+    this.initPicker()
+  },
+  methods: {
+      initPicker() {
+          const fp = flatpickr(this.$refs.input, {
+                dateFormat: 'd.m.Y',
+                onChange: (selectedDates, dateStr) => {
+                    this.$emit('input', dateStr)
+                }
+            })
+        },
+      updatePicker() {
+          fp.setDate(this.value)
+      }
+  }
+}
+</script>
+
+<style>
+</style>
